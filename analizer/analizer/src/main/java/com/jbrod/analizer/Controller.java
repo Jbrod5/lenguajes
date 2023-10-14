bb0d318dfeea2aa05a2fa3904f56ccb26833a4d0
Jbrod5 <jorgebravo202131782@cunoc.edu.gt>
2023-10-14 04:15:16 -0600

    Tokens - ui

---
+++ analizer/analizer/src/main/java/com/jbrod/analizer/Controller.java
@@ -0,0 +1,57 @@
+
+package com.jbrod.analizer;
+
+import com.jbrod.analizer.lexer.Lexer;
+import com.jbrod.analizer.lexer.Token;
+import com.jbrod.analizer.parser.Parser;
+import com.jbrod.analizer.parser.tokens.SyntaxToken;
+import java.io.IOException;
+import java.util.LinkedList;
+
+/**
+ *
+ * @author Jorge
+ */
+public class Controller {
+
+    private Lexer lexer; 
+    private LinkedList<Token> tokens; 
+    
+    private Parser parser; 
+
+    public Controller() {
+        lexer = new Lexer();
+        tokens = new LinkedList<>();
+        
+        parser = new Parser();
+        parser.inicializarAutomatas();
+    }
+    
+    
+    
+    public void analize( String code ) throws IOException{
+        lexer.Analize(code);
+        tokens = lexer.getTokenList();
+        
+        parser.Parse(tokens);
+        
+    }
+
+    
+    public void lex( String code ) throws IOException{
+        lexer.Analize(code);
+        tokens = lexer.getTokenList();
+    }
+    
+    public LinkedList<Token> getTokens() {
+        return tokens;
+    }
+    
+    public LinkedList<SyntaxToken> getGeneralSyntax(){
+        return parser.getSintaxTokens();
+    }
+    
+    public LinkedList<SyntaxToken> getBlocksSyntax(){
+        return parser.getCodeblockTokens();
+    }    
+}
